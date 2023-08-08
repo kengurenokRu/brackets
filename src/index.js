0cfd0281a12a8edbdbc2368e11c02477d0f4358c
kengurenokRu <nastassiadunaeva@gmail.com>
2023-08-08 10:32:33 +0300

    fix: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) 
+{
+  let braskets = new Map(bracketsConfig);
+  let st = [];
+  for (let i=0; i<str.length; i++) { 
+        if (braskets.get(st[st.length-1]) == str[i]) {
+      st.pop(); 
+    } 
+    else 
+    { 
+      st.push(str[i]); 
+    }
+  }
+  if (st.length == 0) return true;
+  else return false; 
 }
